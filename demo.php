eb77ed21546df058cfd850b24ab18bb7fe34248c
hujiangchuan <985840977@qq/com>
2018-05-24 00:31:08 +0800

    demo

--- demo.php
+++ demo.php
@@ -4,22 +4,219 @@
 <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
 <meta name="viewport" content="initial-scale=1, maximum-scale=1, user-scalable=no">
 <title>提交订单-尚新假发定做</title>
+<!--<link type="text/css" href="style/bootstrap-3.3.7-dist/css/bootstrap.css"/>-->
+<link rel="stylesheet" href="//cdn.bootcss.com/bootstrap/3.3.4/css/bootstrap.min.css">
 </head>
 <style type="text/css">
 *{ margin: 0; padding: 0; }
 body{ text-align: center; }
 .box{ width: 100%; max-width: 640px; margin: 0 auto; }
 iframe{width: 100%; border: 0 }
+
+
+
+
+    .header-box{
+        border-bottom: 1px solid #ccc;
+    }
+    .main-box{
+        padding-top: 15px;
+    }
+    .main-box-l{
+        border-right: 1px solid #ccc;
+    }
+    .main-box-l .img-box{
+        min-height: 300px;
+        padding: 15px 0 60px 0;
+    }
+    .main-box-l .img-box img{
+        max-width: 100%;
+        height: auto;
+    }
 </style>
 <body>
 
-<div class="box">
+<div class="box"  style="margin-bottom: 200px;padding-bottom: 200px;border-bottom: 1px solid #ccc;">
   <h2>尚新假发定做提交系统</h2>
 	<img src="tmp/1.jpg" class="bai">
+    <div></div>
+
+
+
+
+
+
+
+
 
 	<script type="text/javascript" src="http://sxdd.sxwig.com/themes/?t=green1&c=4&w=100%&h=414px"></script>
 </div>
+<section class="container ">
+    <div class="header-box">
+        <form>
+            <div class="form-horizontal row">
 
+                <div class="col-sm-6">
+                    <div class="form-group">
+                        <label class="col-sm-3 control-label">店铺名：</label>
+                        <div class="col-sm-9">
+                            <input type="text" class="form-control" placeholder="店铺名">
+                        </div>
+                    </div>
+                    <div class="form-group">
+                        <label for="" class="col-sm-3 control-label">地址：</label>
+                        <div class="col-sm-9">
+                            <input type="text" class="form-control" id="" placeholder="地址">
+                        </div>
+                    </div>
+                    <div class="form-group">
+                        <label for="" class="col-sm-3 control-label">电话：</label>
+                        <div class="col-sm-9">
+                            <input type="text" class="form-control" id="" placeholder="电话">
+                        </div>
+                    </div>
+                </div>
+                <div class="col-sm-6">
+                    <div class="form-group">
+                        <label class="col-sm-3 control-label">交货期：</label>
+                        <div class="col-sm-9">
+                            <input type="text" class="form-control" placeholder="交货期">
+                        </div>
+                    </div>
+                    <div class="form-group">
+                        <label for="" class="col-sm-3 control-label">订单期：</label>
+                        <div class="col-sm-9">
+                            <input type="text" class="form-control" id="" placeholder="订单期">
+                        </div>
+                    </div>
+                    <div class="form-group">
+                        <label for="" class="col-sm-3 control-label">客户姓名：</label>
+                        <div class="col-sm-9">
+                            <input type="text" class="form-control" id="" placeholder="客户姓名">
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </form>
+    </div>
+    <div class="main-box row form-horizontal">
+        <div class="col-sm-6 main-box-l">
+            <h4>客户本人照片</h4>
+            <div class="img-box">
+                <img class="img-rounded" src="https://www.maimai100.cn/static/media/b_03.dbac7caf.png" />
+            </div>
+            <h4>客户要求效果</h4>
+            <div class="img-box">
+                <img class="img-rounded" src="https://www.maimai100.cn/static/media/b_03.dbac7caf.png" />
+            </div>
+        </div>
+        <div class="col-sm-6 main-box-r">
+            <div class="form-group">
+                <label class="col-sm-3 control-label">性别：</label>
+                <div class="col-sm-9 text-left">
+                    <div class="radio">
+                        <label>
+                            <input type="radio" name="1"> 男
+                        </label>
+                        <label>
+                            <input type="radio" name="1"> 女
+                        </label>
+                    </div>
+                </div>
+            </div>
+            <div class="form-group">
+                <label class="col-sm-3 control-label">发型款式：</label>
+                <div class="col-sm-9">
+                    <input type="text" class="form-control" placeholder="发型款式">
+                </div>
+            </div>
+            <div class="form-group">
+                <label for="" class="col-sm-3 control-label">发型颜色：</label>
+                <div class="col-sm-9">
+                    <input type="text" class="form-control" id="" placeholder="发型颜色">
+                </div>
+            </div>
+            <div class="form-group">
+                <label class="col-sm-3 control-label">发量：</label>
+                <div class="col-sm-9 text-left">
+                    <div class="radio">
+                        <label>
+                            <input type="radio" name="1"> 加厚
+                        </label>
+                        <label>
+                            <input type="radio" name="1"> 普通
+                        </label>
+                        <label>
+                            <input type="radio" name="1"> 偏少
+                        </label>
+                    </div>
+                </div>
+            </div>
+            <div class="form-group">
+                <label class="col-sm-3 control-label">挑染：</label>
+                <div class="col-sm-9 text-left">
+                    <div class="radio">
+                        <label>
+                            <input type="radio" name="1"> 是
+                        </label>
+                        <label>
+                            <input type="radio" name="1"> 否
+                        </label>
+                    </div>
+                </div>
+            </div>
+            <div class="form-group">
+                <label class="col-sm-3 control-label">修剪：</label>
+                <div class="col-sm-9 text-left">
+                    <div class="radio">
+                        <label>
+                            <input type="radio" name="1"> 精修
+                        </label>
+                        <label>
+                            <input type="radio" name="1"> 粗剪
+                        </label>
+                        <label>
+                            <input type="radio" name="1"> 成品
+                        </label>
+                        <label>
+                            <input type="radio" name="1"> 毛坯
+                        </label>
+                    </div>
+                </div>
+            </div>
+            <div class="form-group">
+                <label for="" class="col-sm-3 control-label">发长：</label>
+                <div class="col-sm-9">
+                    <input type="text" class="form-control" id="" placeholder="发长">
+                </div>
+            </div>
+            <div class="form-group">
+                <label for="" class="col-sm-3 control-label">头旋用料：</label>
+                <div class="col-sm-9">
+                    <input type="text" class="form-control" id="" placeholder="头旋用料">
+                </div>
+            </div>
+            <div class="form-group">
+                <label for="" class="col-sm-3 control-label">自定义发型：</label>
+                <div class="col-sm-9">
+                    <input type="text" class="form-control" id="" placeholder="自定义发型">
+                </div>
+            </div>
+            <div class="form-group">
+                <label for="" class="col-sm-3 control-label">头旋位置：</label>
+                <div class="col-sm-9">
+                    <input type="text" class="form-control" id="" placeholder="头旋位置">
+                </div>
+            </div>
+            <div class="form-group">
+                <label for="" class="col-sm-3 control-label">备注信息：</label>
+                <div class="col-sm-9">
+                    <textarea class="form-control" style="min-height: 200px;" rows="" cols="">备注信息</textarea>
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
 
 
 </body>
